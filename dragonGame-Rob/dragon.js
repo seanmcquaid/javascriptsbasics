89b8c4bd3b73f129c7e8fef503768c8c446c2788
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-11 09:16:50 -0500

    dragon game completed with rob

--- dragonGame-Rob/dragon.js
+++ dragonGame-Rob/dragon.js
@@ -1,3 +1,30 @@
-document.getElementById("roll-button").addEventListener(click, function(){
-    
-});
+function roll(){
+    // window.alert("I'm an alert") == alert("I'm an alert!")
+    // EVERYTHING belongs to the window
+    // .log on an html thing will show you html
+    // .dir on an html thing will you the html as a js object
+    //  document is an object that belongs to the window which houses the html
+    var rand1 = Math.ceil(Math.random() * 6);
+    var rand2 = Math.ceil(Math.random() * 6);
+    var score = rand1 + rand2;
+    // the dice imaes are dN.gif
+    // we have two random numbers for the N
+    // first ... get the dice html thing
+    var dice = document.getElementById("dice");
+    var die1 = dice.children[0];
+    var die2 = dice.children[1];
+    die1.src = `dragon-assets/d${rand1}.gif`;
+    die2.src = `dragon-assets/d${rand2}.gif`;
+
+    if (score > 8){
+        console.log("you won");
+        var dragon = document.getElementById("dragon");
+        dragon.children[0].src = `dragon-assets/slainDragon.jpg`;
+        dragon.children[0].style.height = "200px";
+        dragon.children[0].style.width = "200px";
+        } else {
+        console.log("you lost");
+    }
+};
+
+document.getElementById("rollButton").addEventListener("click", roll);
